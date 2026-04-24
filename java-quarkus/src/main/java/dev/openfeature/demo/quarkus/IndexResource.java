3712f8dc3d012e0caa0381b7d5fbc28447bb0de5
Simon Schrottner <simon.schrottner@gmail.com>
2026-04-24 18:17:37 +0200

    step/java-quarkus/3.1: add language targeting with inline ImmutableContext

--- java-quarkus/src/main/java/dev/openfeature/demo/quarkus/IndexResource.java
+++ java-quarkus/src/main/java/dev/openfeature/demo/quarkus/IndexResource.java
@@ -2,19 +2,28 @@ package dev.openfeature.demo.quarkus;
 
 import dev.openfeature.sdk.Client;
 import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.ImmutableContext;
 import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.Value;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.HashMap;
 
 @Path("/")
 public class IndexResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public FlagEvaluationDetails<String> helloWorld() {
+    public FlagEvaluationDetails<String> helloWorld(@QueryParam("language") String language) {
         Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getStringDetails("greetings", "No World");
+        HashMap<String, Value> attributes = new HashMap<>();
+        if (language != null && !language.isEmpty()) {
+            attributes.put("language", new Value(language));
+        }
+        return client.getStringDetails("greetings", "No World", new ImmutableContext(attributes));
     }
 }
