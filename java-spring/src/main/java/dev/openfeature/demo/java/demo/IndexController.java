0acfabdf8ecacafe143a3a23d341a294aeeb9e19
Simon Schrottner <simon.schrottner@gmail.com>
2026-04-24 18:18:37 +0200

    step/java-spring/3.1: add language targeting with inline context

--- java-spring/src/main/java/dev/openfeature/demo/java/demo/IndexController.java
+++ java-spring/src/main/java/dev/openfeature/demo/java/demo/IndexController.java
@@ -2,22 +2,26 @@ package dev.openfeature.demo.java.demo;
 
 import dev.openfeature.sdk.Client;
 import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.ImmutableContext;
 import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public FlagEvaluationDetails<String> helloWorld() {
+    public FlagEvaluationDetails<String> helloWorld(@RequestParam(required = false) String language) {
         Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getStringDetails("greetings", "No World");
-    }
-
-    @GetMapping("/error")
-    public FlagEvaluationDetails<Boolean> error() {
-        Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getBooleanDetails("greetings", false);
+        HashMap<String, Value> attributes = new HashMap<>();
+        if (language != null) {
+            attributes.put("language", new Value(language));
+        }
+        ImmutableContext evaluationContext = new ImmutableContext(attributes);
+        return client.getStringDetails("greetings", "No World", evaluationContext);
     }
 }
